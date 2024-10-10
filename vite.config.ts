9d8e0afe9d8da5ffcd81c6a1bcadd85021c35d6e
Daniel <daniel.payne@keldan.co.uk>
2024-10-10 15:17:44 +0100

    updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,22 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore-line
-import { fileURLToPath, URL } from "url"
-
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
+
+import * as path from "node:path"
+import { fileURLToPath } from "node:url"
+
+const filesNeedToExclude = ["**/source_data/**"]
+
+const filesPathToExclude = filesNeedToExclude.map((src) => {
+  return fileURLToPath(new URL(src, import.meta.url))
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
-      },
-    ],
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   server: {
     port: 5080,
